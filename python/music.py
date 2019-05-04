38b43390627cf8a087e50dc23b3196e3182488db
logend2 <logend2@126.com>
2019-05-04 13:02:32 +0800

    还没做好的爬虫

--- python/music.py
+++ python/music.py
@@ -1,176 +1,49 @@
-import md5
-import base64
-import urllib2
-import urllib
+import urllib.request
+import urllib.parse
 import json
-import random
 import os
-import sys
 
-#set cookie
-cookie_opener = urllib2.build_opener()
-cookie_opener.addheaders.append(('Cookie', 'appver=2.0.2'))
-cookie_opener.addheaders.append(('Referer', 'http://music.163.com'))
-urllib2.install_opener(cookie_opener)
+def get(id):
+    list = {}   #歌曲名-歌手，id组成的字典
+    url = ''.join(["http://music.163.com/api/playlist/detail?id=",id])#歌单地址
+    print('正在获取歌单数据...')
+    req = urllib.request.urlopen(url)
+    html = req.read().decode('utf-8')
+    list1 = json.loads(html)    #返回歌单的数据解析
+    x = list1['result']['tracks']
+    for i in x:
+        name = str(i['name'])   #歌名
+        song = str(i['artists'][0]['name']) #歌手
+        id = str(i['id'])   #歌id，下载用
+        x = ''.join([name,r"-",song])#把歌名和歌手拼接成‘歌名-歌手’的格式
+        list[x] = id   #歌名歌手和id组成字典
+    print("共有",len(list),'首歌')
+    return list
 
-def encrypted_id(id):
-    byte1 = bytearray('3go8&$8*3*3h0k(2)2')
-    byte2 = bytearray(id)
-    byte1_len = len(byte1)
-    for i in xrange(len(byte2)):
-        byte2[i] = byte2[i]^byte1[i%byte1_len]
-    m = md5.new()
-    m.update(byte2)
-    result = m.digest().encode('base64')[:-1]
-    result = result.replace('/', '_')
-    result = result.replace('+', '-')
-    return result
-
-def search_artist_by_name(name):
-    search_url = 'http://music.163.com/api/search/get'
-    params = {
-            's': name,
-            'type': 100,
-            'offset': 0,
-            'sub': 'false',
-            'limit': 10
-    }
-    params = urllib.urlencode(params)
-    resp = urllib2.urlopen(search_url, params)
-    artists = json.loads(resp.read())
-    if artists['code'] == 200 and artists['result']['artistCount'] > 0:
-        return artists['result']['artists'][0]
+def downalfile(list):
+    len = 0
+    if os.path.exists("music") == 'fasle':
+        os.mkdir('music')
+        print('no!')
     else:
-        return None
-
-def search_album_by_name(name):
-    search_url = 'http://music.163.com/api/search/get'
-    params = {
-            's': name,
-            'type': 10,
-            'offset': 0,
-            'sub': 'false',
-            'limit': 20
-    }
-    params = urllib.urlencode(params)
-    resp = urllib2.urlopen(search_url, params)
-    resp_js = json.loads(resp.read())
-    if resp_js['code'] == 200 and resp_js['result']['albumCount'] > 0:
-        result = resp_js['result']
-        album_id = 0
-        if result['albumCount'] > 1:
-            for i in range(len(result['albums'])):
-                album = result['albums'][i]
-                print '[%2d]artist:%s\talbum:%s" % (i+1, album["artist"]["name"], album["name"])
-                
-            select_i = int(raw_input('Select One:'))
-            if select_i < 1 or select_i > len(result['albums']):
-                print 'error select'
-                return None
-            else:
-                album_id = select_i-1
-        return result['albums'][album_id]
-    else:
-        return None
-
-
-
-def search_song_by_name(name):
-    search_url = 'http://music.163.com/api/search/get'
-    params = {
-            's': name,
-            'type': 1,
-            'offset': 0,
-            'sub': 'false',
-            'limit': 20
-    }
-    params = urllib.urlencode(params)
-    resp = urllib2.urlopen(search_url, params)
-    resp_js = json.loads(resp.read())
-    if resp_js['code'] == 200 and resp_js['result']['songCount'] > 0:
-        result = resp_js['result']
-        song_id = result['songs'][0]['id']
-        if result['songCount'] > 1:
-            for i in range(len(result['songs'])):
-                song = result['songs'][i]
-                print '[%2d]song:%s\tartist:%s\talbum:%s' % (i+1,song['name'], song['artists'][0]['name'], song['album']['name'])
-            select_i = int(raw_input('Select One:'))
-            if select_i < 1 or select_i > len(result['songs']):
-                print 'error select'
-                return None
-            else:
-                song_id = result['songs'][select_i-1]['id']
-        detail_url = 'http://music.163.com/api/song/detail?ids=[%d]' % song_id
-        resp = urllib2.urlopen(detail_url)
-        song_js = json.loads(resp.read())
-        return song_js['songs'][0]
-    else:
-        return None
-
-def get_artist_albums(artist):
-    albums = []
-    offset = 0
-    while True:
-        url = 'http://music.163.com/api/artist/albums/%d?offset=%d&limit=50' % (artist['id'], offset)
-        resp = urllib2.urlopen(url)
-        tmp_albums = json.loads(resp.read())
-        albums.extend(tmp_albums['hotAlbums'])
-        if tmp_albums['more'] == True:
-            offset += 50
-        else:
-            break
-    return albums
-
-def get_album_songs(album):
-    url = 'http://music.163.com/api/album/%d/' % album['id']
-    resp = urllib2.urlopen(url)
-    songs = json.loads(resp.read())
-    return songs['album']['songs']
-
-def save_song_to_disk(song, folder):
-    name = song['name']
-    fpath = os.path.join(folder, name+'.mp3')
-    if os.path.exists(fpath):
-        return
-
-    song_dfsId = str(song['bMusic']['dfsId'])
-    url = 'http://m%d.music.126.net/%s/%s.mp3' % (random.randrange(1, 3), encrypted_id(song_dfsId), song_dfsId)
-    #print '%s\t%s' % (url, name)
-    #return
-    resp = urllib2.urlopen(url)
-    data = resp.read()
-    f = open(fpath, 'wb')
-    f.write(data)
-    f.close()
-
-def download_song_by_search(name, folder='.'):
-    song = search_song_by_name(name)
-    if not song:
-        print 'Not found ' + name
-        return
-
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    save_song_to_disk(song, folder)
-
-
-def download_album_by_search(name, folder='.'):
-    album = search_album_by_name(name)
-    if not album:
-        print 'Not found ' + name
-        return
+        print("exist!")
     
-    name = album['name']
-    folder = os.path.join(folder, name)
+    for i in list:
+        name = ''.join(['music/',i,'.mp3'])
+        song = urllib.parse.quote(list[i])    #歌名-歌手 中文字符转uel，否则下载会出问题
+        address = ''.join(['http://music.163.com/song/media/outer/url?id=',song,'.mp3'])  #歌曲下载地址
+       # downal.downal(address) #下载进度条失败的尝试
+        print('(oﾟvﾟ)ノ开始下载',i,'...')
 
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    songs = get_album_songs(album)
-    for song in songs:
-        save_song_to_disk(song, folder)
+        data = urllib.request.urlopen(address) #开始下载
+        with open(name,"wb+") as f:
+            f.write(data.read()) #写入磁盘
+            print(i,'下载已完成(￣▽￣)"')
+            len += 1
+    print(len,'首下载成功..')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit(0)
-    download_album_by_search(sys.argv[1], sys.argv[2])
+    print(str(os.path.exists("test_dir")))
+    list = get(input('输入歌单id:\n'))
+    downalfile(list)
+    input("按任意键退出")
